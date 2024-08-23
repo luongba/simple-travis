8f30b363f680803630de360af9f1d5f38dc254b2
luongba <a2k10lnc@gmail.com>
2024-08-23 23:43:35 +0700

    update: update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log('Containers rule em ba xinh gai!');
     await sleep(5000);
   }
 }
